3117b71c5d12da64b3356cb7a17224589e50ec11
akim1995 <a.khalitov@clientlab.ru>
2018-12-21 00:44:58 +0200

    added help for cli version

--- index.js
+++ index.js
@@ -2,7 +2,24 @@ const yargs = require('yargs');
 const newSite = require('./lib.js').newSite;
 const removeSite = require('./lib.js').removeSite;
 const listSites = require('./lib.js').listSites;
-const argv = yargs.argv;
+const argv = yargs
+	.command('create', 'Add new local site', {
+		name: {
+			describe: 'Name of the new site',
+			demand: true,
+			alias: 'n'
+		}
+	})
+	.command('list', 'List all sites')
+	.command('remove', 'Removes specified site', {
+		name: {
+			describe: 'Name of the removed site',
+			demand: true,
+			alias: 'n'
+		}
+	})
+	.help()
+.argv;
 let command = argv._[0];
 
 // console.log('Yargs', argv);
@@ -17,8 +34,8 @@ const serverName = argv.name;
 if (command === 'create' && typeof serverName !== 'undefined') {
 	newSite(serverName );
 } else if(command === 'remove' && typeof serverName !== 'undefined') {
-		removeSite(serverName)
-} else if(command === 'list' || 'ls') {
+		removeSite(serverName);
+} else if(command === 'list') {
     listSites().forEach(site => {
         console.log(site);
     })
